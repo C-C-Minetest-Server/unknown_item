133ffbfea119acf9921be4652b61bedd9f9307d2
Emojigit <yiufamily.hh@gmail.com>
2020-08-05 19:07:02 +0800

    上傳檔案到 ''

---
+++ init.lua
@@ -0,0 +1,5 @@
+minetest.register_craftitem("unknown_item:unknownitem", {
+	description = "Fake Unknown Item",
+	inventory_image = "unknown_item.png",
+})
+minetest.register_alias("fuitem", "unknown_item:unknownitem")
